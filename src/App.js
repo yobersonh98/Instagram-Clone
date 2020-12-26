70f31a8df1b26623e3e633aa5e7a9674da71a976
yobersonh98 <yobersonh98@gmail.com>
2020-12-26 18:46:08 -0400

    Primer commit de la app clone de instagram

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Post from "./components/Post";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div className="app__header">
+          <img
+            className="app__headerImage"
+            src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+            alt=""
+          />
+        </div>
+
+        <Post />
+      </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Post.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../css/Post.css";
+
+const Post = () => {
+  return (
+    <div className="post">
+      <h3>Username</h3>
+
+      <img
+        className="post__image"
+        src="https://miro.medium.com/max/3840/0*oZLL-N4dGNlBe4Oh.png"
+      />
+      {/* image */}
+      {/* username + caption */}
+    </div>
+  );
+};
+
+export default Post;
